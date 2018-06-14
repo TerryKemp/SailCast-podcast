06d93e235b6ed71468e7427ba60e86db348a806d
Terry Kemp <Terry.kemp14@gmail.com>
2018-06-14 16:38:45 -0400

    .

--- index.js
+++ index.js
@@ -82,6 +82,9 @@ app.use((req, res, next) => {
 })
 
 app.use(routes)
-app.listen(env.port, () => console.log(`Express is listening on port ${env.port}`))
+  app.set('port', process.env.PORT || 3001)
 
+app.listen(app.get('port'), () => {
+  console.log(`✅ PORT: ${app.get('port')} 🌟`)
+})
 app.use(express.static(`${__dirname}/public`))
