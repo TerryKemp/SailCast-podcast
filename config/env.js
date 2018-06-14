7f32fb48730867b87ae4047a1e0c6d870a97e0e9
Terry Kemp <Terry.kemp14@gmail.com>
2018-06-14 16:20:09 -0400

    .

--- config/env.js
+++ config/env.js
@@ -1,4 +1,7 @@
 module.exports = {
-  db: process.env.MONGODB_URI || 'mongodb://localhost/sailcast',
-  port: process.env.PORT || 3000
-}
+//   db: process.env.MONGODB_URI || 'mongodb://localhost/sailcast',
+//   port: process.env.PORT || 3000
+// }
+ db: process.env.MLAB_URL || 'mongodb://localhost/sailcast',
+   port: process.env.PORT || 3000
+ }
