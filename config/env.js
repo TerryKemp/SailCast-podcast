6191085397a3b3c7c9127cb92b049b6e64712288
Terry Kemp <Terry.kemp14@gmail.com>
2018-06-20 09:43:03 -0400

    .

--- config/env.js
+++ config/env.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose')
 
-module.exports = {
-  db: process.env.MONGODB_URI || 'mongodb://localhost/sailcast',
-  port: process.env.PORT || 3001
+// module.exports = {
+//   db: process.env.MONGODB_URI || 'mongodb://localhost/sailcast',
+//   port: process.env.PORT || 3001
+// }
+
+if (process.env.NODE_ENV === 'production') {
+  mongoose.connect(process.env.MLAB_URL)
+} else {
+  mongoose.connect('mongodb://localhost/sailcast')
 }
