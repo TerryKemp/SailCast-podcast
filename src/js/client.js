a263399483e7315c6b51c0140f020ca9cf2700dd
Terry Kemp <Terry.kemp14@gmail.com>
2018-06-20 10:57:32 -0400

    removed init2

--- src/js/client.js
+++ src/js/client.js
@@ -1,5 +1,4 @@
 $(init)
-$(init2)
 
 function init () {
   if ($('.podcast-container').length !== 0) getTopPodcasts()
@@ -19,24 +18,6 @@ function getTopPodcasts () {
       })
     })
 }
-// function init2 () {
-//   if ($('.podcast-container').length !== 0) getTopPodcasts2()
-//   $('.search-podcasts').on('submit', searchForPodcasts)
-//   $('.podcasts-search-results').on('click', 'div', postPodcastToPlaylist)
-// }
-
-// function getTopPodcasts2 () {
-//   $
-//     .get('https://itunes.apple.com/us/rss/toppodcasts/genre=1321/json')
-//     .done(data => {
-//       const json = JSON.parse(data)
-//       json.feed.entry.forEach(podcast => {
-//         $(`<div =class"col-3">
-//       <img src="${podcast['im:image'][2].label}">
-//       </div>`).appendTo('.podcast-container')
-//       })
-//     })
-// }
 
 function searchForPodcasts (e) {
   e.preventDefault()
